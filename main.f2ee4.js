cfd43e62b3c1325fea70ad338b7362004f735610
276315136 <276315136@qq.com>
2018-11-07 11:17:08 +0800

    add start scene  tip---------

--- main.f2ee4.js
+++ main.f2ee4.js
@@ -155,7 +155,7 @@
         var jsList = settings.jsList;
 
         if (!false) {
-            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
+            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.38d9a.js';
             if (jsList) {
                 jsList = jsList.map(function (x) {
                     return 'src/' + x;
@@ -203,7 +203,7 @@
     }
 
     if (false) {
-        require(window._CCSettings.debug ? 'cocos2d-js.js' : 'cocos2d-js-min.js');
+        require(window._CCSettings.debug ? 'cocos2d-js.js' : 'cocos2d-js-min.af7c8.js');
         require('./libs/weapp-adapter/engine/index.js');
         var prevPipe = cc.loader.md5Pipe || cc.loader.assetLoader;
         cc.loader.insertPipeAfter(prevPipe, wxDownloader);
@@ -212,7 +212,7 @@
     }
 
     if (window.jsb) {
-        require('src/settings.js');
+        require('src/settings.20d31.js');
         require('src/jsb_polyfill.js');
         boot();
         return;
@@ -224,7 +224,7 @@
 
         var cocos2d = document.createElement('script');
         cocos2d.async = true;
-        cocos2d.src = window._CCSettings.debug ? 'cocos2d-js.js' : 'cocos2d-js-min.js';
+        cocos2d.src = window._CCSettings.debug ? 'cocos2d-js.js' : 'cocos2d-js-min.af7c8.js';
 
         var engineLoaded = function () {
             document.body.removeChild(cocos2d);
